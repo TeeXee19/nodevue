555c30a6d5630395ddc16dee7a8e7bd6ed51bd86
Tochukwu Franklin Ene <teec3000@gmail.com>
2021-04-13 08:10:36 +0100

    Iniital Commit, Working on the tutorial model

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const PORT = process.env.PORT || 4000;
+
+const app = express()
+
+var corsOptions = {
+	origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
+
+app.use(bodyParser.json());
+
+const db = require("./app/models");
+db.sequelize.sync({ force: true}).then(()=>{
+	console.log("DROP AND RE-SYNC DB.");
+})
+
+app.get("/", (req, res)=>{
+	res.json({message: "Willkommen zu Frank Application"})
+});
+
+app.listen(PORT, ()=>{
+	console.log(`Server is running fine on ${PORT}.`);
+});
+
+
