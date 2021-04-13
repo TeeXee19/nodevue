713c1253176d1797b29d5565b02508d64ada2894
Tochukwu Franklin Ene <teec3000@gmail.com>
2021-04-13 08:31:56 +0100

    Worked on the tutorial controller

--- app/models/tutorial.model.js
+++ app/models/tutorial.model.js
@@ -1,3 +1,14 @@
 module.exports = (sequelize, Sequelize) =>{
-	const Tutorial = 
+	const Tutorial = sequelize.define("tutorial", {
+		title: {
+			type: Sequelize.STRING
+		},
+		description: {
+			type: Sequelize.STRING
+		},
+		published: {
+			type: Sequelize.BOOLEAN
+		}
+	});
+	return Tutorial;
 }
